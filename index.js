9f392803278a82bbcaf585783bd67dd7c47a6bdc
ASDFSD <40294643+yutaka010@users.noreply.github.com>
2019-05-20 20:02:57 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -16,38 +16,38 @@
 //   });
 // });
 $(function() {
-
-    $('.move_right').delay(400).fadeIn(400)
-    $('.move_left').delay(400).fadeIn(400)
-    $(".move_left").animate({
-        "padding-right": "50px"
-    });
-    $(".move_right").animate({
-        "padding-left": "50px"
-    });
-
+  $('.move_right').delay(400).fadeIn(400)
+  $('.move_left').delay(400).fadeIn(400)
+  $(".move_left").animate({
+    "padding-right": "50px"
+  });
+  $(".move_right").animate({
+    "padding-left": "50px"
+  });
   $('li:nth-child(1)').hover(function() {
-
-    //ボタンの横に要素を作成
     $(this).css('width', '200%');
-    $('.text1').css('opacity', '1');
+    $('.text1').css( 'opacity', '1');
     $('.black2').css('background-color', 'RGBA(0,0,0,0.5)');
   }, function() {
-
-    //色指定を空欄にすれば元の色に戻る
+    //初期化
     $(this).css('width', '');
     $('.text1').css('opacity', '');
     $('.black2').css('background-color', '');
   });
+  $(function(){
+    $('.text').children().addBack().contents().each(function() {
+        $(this).replaceWith($(this).text().replace(/(\S)/g, '<span class="text-move">$&</span>'));
+    });
+     setTimeout(function(){
+        $(".text").addClass("active");
+    },100);
+  });
   $('li:nth-child(2)').hover(function() {
-
-    //ボタンの横に要素を作成
     $(this).css('width', '200%');
     $('.text2').css('opacity', '1');
     $('.black1').css('background-color', 'RGBA(0,0,0,0.5)');
   }, function() {
-
-    //色指定を空欄にすれば元の色に戻る
+    //初期化
     $(this).css('width', '');
     $('.text2').css('opacity', '');
     $('.black1').css('background-color', '');
